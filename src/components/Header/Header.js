218d842ed9c05d9701544c77c989edb989020ad4
avsssai <avsssai@gmail.com>
2022-04-06 15:37:00 +0530

    lesson 2 completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,70 +1,132 @@
-import React from 'react';
-import styled from 'styled-components/macro';
-import { Menu, Search, User } from 'react-feather';
+import React from "react";
+import styled from "styled-components/macro";
+import { Menu, Search, User } from "react-feather";
 
-import { QUERIES } from '../../constants';
+import { COLORS, QUERIES } from "../../constants";
 
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import Logo from '../Logo';
-import Button from '../Button';
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import Logo from "../Logo";
+import Button from "../Button";
 
 const Header = () => {
-  return (
-    <header>
-      <SuperHeader>
-        <Row>
-          <ActionGroup>
-            <button>
-              <Search size={24} />
-            </button>
-            <button>
-              <Menu size={24} />
-            </button>
-          </ActionGroup>
-          <ActionGroup>
-            <button>
-              <User size={24} />
-            </button>
-          </ActionGroup>
-        </Row>
-      </SuperHeader>
-      <MainHeader>
-        <Logo />
-      </MainHeader>
-    </header>
-  );
+	return (
+		<header>
+			<SuperHeader>
+				<Row>
+					<ActionGroup>
+						<button>
+							<Search size={24} />
+						</button>
+						<button>
+							<Menu size={24} />
+						</button>
+					</ActionGroup>
+					<ActionGroup>
+						<button>
+							<User size={24} />
+						</button>
+					</ActionGroup>
+				</Row>
+			</SuperHeader>
+			<MainHeader>
+				<DesktopActionGroup>
+					<button>
+						<Search size={24} />
+					</button>
+					<button>
+						<Menu size={24} />
+					</button>
+				</DesktopActionGroup>
+				<Logo />
+				<SubscribeWrapper>
+					<Button>Subscribe</Button>
+					<Link>Already a Subscriber?</Link>
+				</SubscribeWrapper>
+			</MainHeader>
+		</header>
+	);
 };
 
 const SuperHeader = styled.div`
-  padding: 16px 0;
-  background: var(--color-gray-900);
-  color: white;
+	padding: 16px 0;
+	background: var(--color-gray-900);
+	color: white;
+	@media ${QUERIES.laptopAndUp} {
+		display: none;
+	}
 `;
 
 const Row = styled(MaxWidthWrapper)`
-  display: flex;
-  justify-content: space-between;
+	display: flex;
+	justify-content: space-between;
 `;
 
 const ActionGroup = styled.div`
-  display: flex;
-  gap: 24px;
+	display: flex;
+	gap: 24px;
 
-  /*
+	/*
     FIX: Remove the inline spacing that comes with
     react-feather icons.
   */
-  svg {
-    display: block;
-  }
+	svg {
+		display: block;
+	}
+`;
+
+const DesktopActionGroup = styled(ActionGroup)`
+	display: none;
+	@media ${QUERIES.laptopAndUp} {
+		display: flex;
+	}
+`;
+const SubscribeWrapper = styled.div`
+	display: none;
+	justify-self: end;
+	/* align-self: end; //simple solution for center aligned elements in row */
+	position: relative;
+	@media ${QUERIES.laptopAndUp} {
+		/* display: flex;
+		flex-direction: column; */
+		display: revert;
+	}
+`;
+
+const Link = styled.a`
+	text-decoration: underline;
+	width: 100%;
+	position: absolute;
+	font-style: italic;
+	/* text-align: center; */
+	margin-top: 8px;
+	color: ${COLORS.gray[900]};
+	text-align: center;
+	cursor: pointer;
+	&:hover {
+		text-decoration: none;
+	}
 `;
 
 const MainHeader = styled(MaxWidthWrapper)`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-top: 32px;
-  margin-bottom: 48px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	margin-top: 32px;
+	margin-bottom: 48px;
+
+	@media ${QUERIES.tabletAndUp} {
+		margin-top: 48px;
+		margin-bottom: 72px;
+	}
+
+	@media ${QUERIES.laptopAndUp} {
+		justify-content: start;
+		align-items: center;
+		display: grid;
+		grid-template-columns: 1fr auto 1fr;
+		margin-top: 16px;
+		margin-botton: 72px;
+	}
 `;
 
 export default Header;
--- src/components/OpinionStory/OpinionStory.js
+++ src/components/OpinionStory/OpinionStory.js
@@ -1,43 +1,58 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
+import { QUERIES } from "../../constants";
 
 const OpinionStory = ({ id, title, author, avatar }) => {
-  return (
-    <a href={`/story/${id}`}>
-      <Wrapper>
-        <Avatar alt="" src={avatar} />
-        <div>
-          <AuthorName>{author}</AuthorName>
-          <ArticleTitle>{title}</ArticleTitle>
-        </div>
-      </Wrapper>
-    </a>
-  );
+	return (
+		<a href={`/story/${id}`}>
+			<Wrapper>
+				<Avatar alt='' src={avatar} />
+				<StoryWrapper>
+					<AuthorName>{author}</AuthorName>
+					<ArticleTitle>{title}</ArticleTitle>
+				</StoryWrapper>
+			</Wrapper>
+		</a>
+	);
 };
 
 const Wrapper = styled.article`
-  color: var(--color-gray-900);
+	color: var(--color-gray-900);
+	/* display: flex;
+	flex-direction: row-reverse; */
+
+	@media ${QUERIES.tabletOnly} {
+		float: revert;
+		margin-left: revert;
+	}
 `;
 
 const Avatar = styled.img`
-  display: block;
-  width: 48px;
-  height: 48px;
-  border-radius: 50%;
-  object-fit: cover;
+	display: block;
+	width: 48px;
+	height: 48px;
+	border-radius: 50%;
+	object-fit: cover;
+	float: right;
+	margin-left: 16px;
+	/* align-self: center; */
+`;
+
+const StoryWrapper = styled.div`
+	flex: 1;
 `;
 
 const AuthorName = styled.p`
-  font-size: 1.125rem;
-  font-weight: var(--font-weight-medium);
-  color: var(--color-gray-700);
-  margin-bottom: 4px;
+	font-size: 1.125rem;
+	font-weight: var(--font-weight-medium);
+	color: var(--color-gray-700);
+	margin-bottom: 4px;
 `;
 
 const ArticleTitle = styled.h3`
-  font-size: 1.125rem;
-  font-weight: var(--font-weight-bold);
-  line-height: 1.3;
+	font-size: 1.125rem;
+	font-weight: var(--font-weight-bold);
+	line-height: 1.3;
 `;
 
 export default OpinionStory;
